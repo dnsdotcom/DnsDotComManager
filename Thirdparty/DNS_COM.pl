229ef2dd112cb171b38ba6192fb06ef63aad892f
Miller Hooks <me@millerhooks.com>
2010-01-20 13:47:59 -0800

    implamented a few functions from the API in perl, created template, and cpanel pluging

---
+++ Thirdparty/DNS_COM.pl
@@ -0,0 +1,98 @@
+#!/usr/bin/perl -w
+package	Cpanel::Thirdparty::DNS_COM;
+
+use strict;
+use warnings; 
+use Data::Dumper;
+
+use LWP::Simple;
+use JSON;
+
+my $cmd	= '';
+
+sub dns_query{
+    $cmd            = $_[0];
+    my $username    = 'me@millerhooks.com';
+    my $password    = 'sucka1411';
+    my $hostname    = 'sandbox.comwired.com';
+    my $query	    = "http://$hostname/api/$cmd/?email=$username&password=$password";
+    
+    my $json = new JSON;
+    my $content = get $query;
+    my $domain_data = $json->allow_nonref->utf8->relaxed->decode($content);
+    die "Couldn't get $query" unless defined $content;
+    return $domain_data;
+}
+
+sub getDomains{
+    $cmd = 'getDomains';
+    my $domain_data = dns_query($cmd);
+    my %domain_hash = ();
+    
+    if ($domain_data->{meta}->{success} == 0){
+        my $meta->{error}   = $error = $domain_data->{meta}->{error};
+        my $meta->{success} = $domain_data->{meta}->{success};
+        return my $meta;
+    }else{
+        my $i = 0;
+        foreach my $domain(@{$domain_data->{data}}){
+            $domain_hash{$i}->{id}                    = $domain->{id};
+            $domain_hash{$i}->{name}                  = $domain->{name};
+            $domain_hash{$i}->{mode}                  = $domain->{mode};
+            $domain_hash{$i}->{date_created}          = $domain->{date_created};
+            $domain_hash{$i}->{date_last_modified}    = $domain->{date_last_modified};
+            $domain_hash{$i}->{num_hosts}             = $domain->{num_hosts};
+            
+            print "DOMAIN: $domain_hash{$i}->{name} \n";
+            $i++;
+        }
+        return $domain_hash;
+    }
+}
+
+sub getDomainGroups {
+    $cmd = 'getDomainGroups';
+    my $domain_data = dns_query($cmd);
+    my %domain_hash = ();
+    
+    if ($domain_data->{meta}->{success} == 0){
+        my $meta->{success} = $domain_data->{meta}->{success};
+        return my $meta;
+    }else{
+        my $i = 0;
+        foreach my $domain(@{$domain_data->{data}}){
+            $domain_hash{$i}->{id}                    = $domain->{id};
+            $domain_hash{$i}->{name}                  = $domain->{name};
+            $domain_hash{$i}->{date_created}          = $domain->{date_created};
+            $domain_hash{$i}->{num_domains}           = $domain->{num_domains};
+            
+            print "DOMAIN GROUP: $domain_hash{$i}->{name} \n";
+            $i++;
+        }
+        return $domain_hash;
+    }
+}
+
+sub getDomainsInGroup {
+    $cmd = 'getDomainsInGroup';
+    my $domain_data = dns_query($cmd);
+    my %domain_hash = ();
+    
+    if ($domain_data->{meta}->{success} == 0){
+        my $meta->{error}   = $error = $domain_data->{meta}->{error};
+        my $meta->{success} = $domain_data->{meta}->{success};
+        return my $meta;
+    }else{
+        my $i = 0;
+        foreach my $domain(@{$domain_data->{data}}){
+            $domain_hash{$i}->{id}                    = $domain->{id};
+            $domain_hash{$i}->{name}                  = $domain->{name};
+            $domain_hash{$i}->{date_created}          = $domain->{date_created};
+            $domain_hash{$i}->{date_last_modified}    = $domain->{date_last_modified};
+            
+            print "DOMAINS IN GROUP: $domain_hash{$i}->{name} \n";
+            $i++;
+        }
+        return $domain_hash;
+    }
+}
